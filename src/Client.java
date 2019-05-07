7af86e5d681f4cfd16070d1d64bdfba617d95813
AnderskdKnudsen <anderskdknudsen@gmail.com>
2019-05-07 12:43:32 +0200

    hadsh

--- src/Client.java
+++ src/Client.java
@@ -1,78 +1,65 @@
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
-/**
- * Created by den udvalgte on 19-09-2017.
- */
 public class Client
 {
-
     private String clientName;
     private InetAddress host;
     private int PORT = 1235;
     Scanner userEntry, input;
     PrintWriter output;
 
+    private Socket link;
 
-    public Client()
+
+    public Client(String host, int PORT)
     {
-        System.out.println("Welcome to the auction! Please type your name:");
-        userEntry = new Scanner(System.in);
-        clientName = userEntry.nextLine();
+        getClientName();
 
         try
         {
-            host = InetAddress.getLocalHost();
+            this.host = InetAddress.getByName(host);
+            this.PORT = PORT;
         }
         catch(UnknownHostException uhEx)
         {
             System.out.println("Host ID not found!");
             System.exit(1);
         }
-        accessServer();
-
     }
 
-    private void accessServer()
-    {
-        Socket link = null;
+    private void getClientName(){
+        System.out.println("Welcome to the auction! Please type your name:");
+        userEntry = new Scanner(System.in);
+        clientName = userEntry.nextLine();
+    }
 
+    public void createConnection()
+    {
         try
         {
-            link = new Socket(host,PORT);
-            System.out.println("Thank you, " + clientName + "\nYou are now connected to the auction.");
-
+            link = new Socket(host, PORT);
             input = new Scanner(link.getInputStream());
 
-            output = new PrintWriter(link.getOutputStream(),true);
-
-            output.println(clientName);
-
-            String fromServer = input.nextLine();
-            System.out.println(fromServer);
-
-            String toServer = userEntry.nextLine();
-            System.out.println(toServer);
-            output.println(toServer);
-
-
-
+            output = new PrintWriter(link.getOutputStream(), true);
+            System.out.println("Thank you, " + clientName + "\nYou are now connected to the auction.");
         }
         catch(IOException ioEx)
         {
             ioEx.printStackTrace();
         }
+    }
 
-        finally
-        {
+    public void closeConnection(){
+
             try
             {
-                System.out.println(
-                        "\n* Closing connection... *");
+                System.out.println("\n* Closing connection... *");
                 link.close();					//Step 4.
             }
             catch(IOException ioEx)
@@ -80,6 +67,33 @@ public class Client
                 System.out.println("Unable to disconnect!");
                 System.exit(1);
             }
+
+    }
+
+    public void run(){
+        try{
+            output.println(clientName);
+            while(true)
+            {
+                String fromServer = input.nextLine();
+                System.out.println(fromServer);
+
+                while(!userEntry.hasNextInt() || userEntry.nextInt() < 0){
+                    System.out.println("Please type a valid, positive integer");
+                }
+
+                String toServer = userEntry.nextLine();
+
+                output.println(toServer);
+
+                if(fromServer.equals("nu lukker vi lortet")) {
+                    closeConnection();
+                    break;
+                }
+            }
+        } catch (Exception e){
+            e.printStackTrace();
         }
+
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
+import java.net.InetAddress;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        Client client = new Client();
+        Client client = new Client("192.168.1.19", 1235);
+        client.createConnection();
+        client.run();
     }
 }
